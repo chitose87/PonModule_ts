28ce1cc463fe9a526a0b7587d5f87ad14b7fbbc6
pon87 <chitose87@gmail.com>
2018-01-16 17:39:15 +0900

    setting change

--- PonModule.ts
+++ PonModule.ts
@@ -1,74 +1,87 @@
-import * as $ from "jquery";
-import {AsEventDispatcher} from "../AsEvent/AsEventDispatcher";
-import {PonModuleEvent} from "./PonModuleEvent";
+///<reference path="../asevent/AsEventDispatcher.ts"/>
 
-export class PonModule extends AsEventDispatcher {
-	$obj: JQuery;
+namespace pon87.modules {
 
-	constructor(htmlPath: string, cssPath: string) {
-		super();
-		// this._dir = htmlPath.match("^.*/")[0];
+	/**
+	 * @version 0.0.1
+	 * @author chitose
+	 */
+	export class PonModule extends event.AsEventDispatcher {
+		$obj: JQuery;
 
-		$.ajax(<JQuery.AjaxSettings>{url: htmlPath, datatype: "html"})
-			.then((data) => {
-				let v = $($.parseHTML(data));
-				this.$obj.replaceWith(v);
-				this.$obj = v;
+		constructor(htmlPath: string, cssPath: string) {
+			super();
+			// this._dir = htmlPath.match("^.*/")[0];
 
-				// include modules
-				this.$obj.find("module").each((i, ele) => {
-					let $tar = $(ele);
-					requirejs([$tar.html()], (data: any) => {
-						for (let i in data) {
-							try {
-								let ponModule: PonModule = new data[i]();
-								$tar.replaceWith(ponModule.$obj);
-								break;
-							} catch (e) {
+			$.ajax(<JQuery.AjaxSettings>{url: htmlPath, datatype: "html"})
+				.then((data) => {
+					let v = $($.parseHTML(data));
+					this.$obj.replaceWith(v);
+					this.$obj = v;
+
+					// include modules
+					this.$obj.find("module").each((i, ele) => {
+						let $tar = $(ele);
+						requirejs([$tar.html()], (data: any) => {
+							for (let i in data) {
+								try {
+									let ponModule: PonModule = new data[i]();
+									$tar.replaceWith(ponModule.$obj);
+									break;
+								} catch (e) {
+								}
 							}
-						}
-					})
+						})
+					});
+					this.createdView();
+				}, () => {
+
 				});
-				this.createdView();
-			}, () => {
 
-			});
+			$.ajax(<JQuery.AjaxSettings>{
+				url: cssPath
+				, datatype: "text"
+			})
+				.then((data) => {
+					data = data.replace(/\r?\n/g, '');
+					let list = data.split(/; ?}/);
 
-		$.ajax(<JQuery.AjaxSettings>{
-			url: cssPath
-			, datatype: "text"
-		})
-			.then((data) => {
-				data = data.replace(/\r?\n/g, '');
-				let list = data.split(/; ?}/);
-
-				let newStyle = document.createElement('style');
-				newStyle.type = "text/css";
-				document.getElementsByTagName('head').item(0).appendChild(newStyle);
-				let css: any = document.styleSheets.item(0);
-				let idx = css.cssRules.length;
-				for (let ele of list) {
-					try {
-						css.insertRule(ele + ";}", idx);
-						idx++;
-					} catch (e) {
+					let newStyle = document.createElement('style');
+					newStyle.type = "text/css";
+					document.getElementsByTagName('head').item(0).appendChild(newStyle);
+					let css: any = document.styleSheets.item(0);
+					let idx = css.cssRules.length;
+					for (let ele of list) {
+						try {
+							css.insertRule(ele + ";}", idx);
+							idx++;
+						} catch (e) {
+						}
 					}
-				}
-				this.createdStyle();
-			}, () => {
+					this.createdStyle();
+				}, () => {
 
-			});
+				});
 
-		this.$obj = $("<div>")
-			.attr("data-pon", "PonModule")
-		;
-	}
+			this.$obj = $("<div>")
+				.attr("data-pon", "PonModule")
+			;
+		}
 
-	protected createdView(): PonModule {
-		return this.dispatchEvent(new PonModuleEvent(PonModuleEvent.CREATED_VIEW));
-	}
+		/**
+		 *
+		 * @returns {PonModule}
+		 */
+		protected createdView(): PonModule {
+			return this.dispatchEvent(new PonModuleEvent(PonModuleEvent.CREATED_VIEW));
+		}
 
-	protected createdStyle(): PonModule {
-		return this.dispatchEvent(new PonModuleEvent(PonModuleEvent.CREATED_STYLE));
+		/**
+		 *
+		 * @returns {PonModule}
+		 */
+		protected createdStyle(): PonModule {
+			return this.dispatchEvent(new PonModuleEvent(PonModuleEvent.CREATED_STYLE));
+		}
 	}
 }
--- PonModuleEvent.ts
+++ PonModuleEvent.ts
@@ -1,6 +1,15 @@
-import {AsEvent} from "../AsEvent/AsEvent";
+///<reference path="../asevent/AsEvent.ts"/>
 
-export class PonModuleEvent extends AsEvent {
-    static CREATED_VIEW: string = "createdView";
-    static CREATED_STYLE: string = "createdStyle";
+namespace pon87.modules {
+	import AsEvent = event.AsEvent;
+
+	/**
+	 * @version 0.0.1
+	 * @author chitose
+	 */
+	export class PonModuleEvent extends AsEvent {
+		static CREATED_VIEW: string = "createdView";
+		static CREATED_STYLE: string = "createdStyle";
+	}
+
 }
